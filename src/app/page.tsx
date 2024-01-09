570e7d3a32b162d2154a2e2944a92de7466fbe1c
Rajarshi-Misra <rajarshimisra2100@gmail.com>
2024-01-09 15:36:22 +0530

    Attempts with Alchemy SDK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import React, { useState, useEffect } from "react";
 const { ethers } = require("ethers");
+import { Alchemy, Network } from "alchemy-sdk";
 
 export default function Home() {
+  const config = {
+    apiKey: "Em4-KgltWqKptYp1dOvY9Ohk-YSSfo2k",
+    network: Network.ETH_MAINNET,
+  };
+  const [change, setChange] = useState(0);
+  const alchemy = new Alchemy(config);
   const [balance, setBalance] = useState(0);
   const [provider, setProvider] = useState(null);
 
   useEffect(() => {
     const initializeProvider = async () => {
+      const data = await alchemy.core.getTokenBalances(
+        "0x7afb9de72A9A321fA535Bb36b7bF0c987b42b859",
+        ["0x91a1b0247df9B803Dcfa820522711E456909Ac80"]
+      );
+
+      console.log("Token balance for Address");
+      console.log(data);
       if (window.ethereum) {
         await window.ethereum.request({ method: "eth_requestAccounts" });
         const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -31,6 +45,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
       <div className="text-3xl">Chain:Kroma</div>
       <div className="text-6xl">{balance} KRO</div>
+      <div className="text-3xl text-green-500">{change}%</div>
       <div className="text-3xl">
         Contract Address:0x7afb9de72A9A321fA535Bb36b7bF0c987b42b859
       </div>
